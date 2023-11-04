696b8b42a588f70c169f5e583eb4852e45a148c4
Brandon Lewallen <lewallenbrandon@gmail.com>
2023-11-04 17:43:19 -0500

    update the init file

--- lua/lewalbrand/init.lua
+++ lua/lewalbrand/init.lua
@@ -1,7 +1,1 @@
-require('lewalbrand.set')
-require('lewalbrand.remap')
-require('lewalbrand.command')
--- require('lewalbrand.which-key')
-require('lewalbrand.gitsigns')
-require('lewalbrand.tabby')
 require('lewalbrand.packer')
