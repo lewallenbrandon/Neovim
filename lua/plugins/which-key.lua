33a8d62e4348d08ef87906afb8bf738917359465
Brandon Lewallen <lewallenbrandon@gmail.com>
2025-01-15 20:36:15 -0600

    Adding plantuml support to config

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -6,13 +6,4 @@ return {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
   },
-  keys = {
-    {
-      "<leader>?",
-      function()
-        require("which-key").show({ global = false })
-      end,
-      desc = "Buffer Local Keymaps (which-key)",
-    },
-  },
 }
